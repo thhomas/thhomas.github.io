e2c1fa1da0b551643a463ad2e1ed5bf7384462cf
admin <admin@Nomade-7.local>
2016-02-11 12:57:49 +0100

    update

--- js/example-1.js
+++ js/example-1.js
@@ -10,13 +10,19 @@
 angular
   .module('example1', [
     'angularSpinner',
-    'ign.angular.control',
     'ign.angular.layer',
     'ign.angular.map',
     'ign.angular.geocoder'
   ])
   .controller('MainCtrl', ['$rootScope', '$scope', '$location', '$http', '$timeout', 'Layer', 'LayerService', 'usSpinnerService', function($rootScope, $scope, $location, $http, $timeout, Layer, LayerService, usSpinnerService) {
+  $scope.layerIds = ['google', 'mapbox'];
+  $scope.displayMode = 'doubleMap';
 	    	
-  	
+  $scope.$on('layerAdded', function(event, layer, index, mapId) {
+    if(index === '1') {
+      $scope.getMap().initDisplayControls($scope.displayMode);
+    }
+  });
+
  
 }]);
